55d095bc1a5e07ce3ae97ef5ec0c9a9a6f8796e3
Jake Lee <jkjalee@gmail.com>
2025-05-11 15:01:21 -0700

    Progress

---
+++ pytestsDemo/test_demo1.py
@@ -0,0 +1,18 @@
+# Any pytest file should start with test_ or end with _test
+# pytest method names should start with test
+# Any code should be wrapped in method only
+
+'''
+To run all tests in a directory, go that directory and in the terminal call:
+    - py.test
+    - py.test -v
+    - py.test -v -s
+'''
+
+
+
+def test_firstProgram():
+    print('Helloo')
+
+def test_firstProgram():
+    print('Good morning')
---
+++ pytestsDemo/test_demo2.py
@@ -0,0 +1,4 @@
+def test_firstProgram():
+    msg = 'Hello'
+    assert msg == 'Hi', "Test failed because strings do not match"
+
