c0c0782ea87b0f5ea0461326ee7f8a6d95013004
Jake Lee <jkjalee@gmail.com>
2025-05-14 23:55:38 -0700

    Update pom

--- pageObjects/login.py
+++ pageObjects/login.py
@@ -9,7 +9,7 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def login(self, username="rahulshettyacademy", password="learning"):
+    def login(self, username, password):
         self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
         self.driver.find_element(*self.SIGNIN_BUTTON).click()
--- pytestsDemo/test_e2eTestFramework.py
+++ pytestsDemo/test_e2eTestFramework.py
@@ -1,30 +1,38 @@
+import json
+import pytest
 from pageObjects.cart import Cart
 from pageObjects.checkoutPage import CheckoutPage
 from pageObjects.homePage import HomePage
 from pageObjects.login import LoginPage
 from pageObjects.shopPage import ShopPage
 
+test_data_path = '../data/test_e2eTestFramework.json'
+with open(test_data_path) as f:
+    test_data = json.load(f)
+    test_list = test_data["data"]
 
-def test_e2e(browserInstance):
+
+@pytest.mark.parametrize("test_list_item", test_list)
+def test_e2e(browserInstance, test_list_item):
     driver = browserInstance
     # driver.get('https://rahulshettyacademy.com/loginpagePractise/')
     driver.get("https://rahulshettyacademy.com/angularpractice/")
 
     # loginPage = LoginPage(driver)
-    # loginPage.login()
+    # loginPage.login(test_list_item['userEmail'], test_list_item['userPassword'])
 
     homePage = HomePage(driver)
     homePage.go_to_shop_page()
 
     shopPage = ShopPage(driver)
-    shopPage.add_product_to_cart('iphone x')
+    shopPage.add_product_to_cart(test_list_item['productName'])
     shopPage.go_to_cart()
 
     cart = Cart(driver)
     cart.click_checkout_button()
 
     checkoutPage = CheckoutPage(driver)
-    checkoutPage.select_country()
+    checkoutPage.select_country(test_list_item['country'])
     checkoutPage.click_checkbox()
     checkoutPage.click_purchase_button()
     checkoutPage.verify_success_message()
